8a210d8ccd1cb292e70aa0637b1d2847be9258a6
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-07-13 22:32:00 -0300

    Create Search Mechanism

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -16,30 +16,28 @@ $(document).ready(function () {
         .then(function (response) {
             console.log(response);
 
-            var map = L.map('map', {
-                zoomSnap: 0.2,
-                gestureHandling: true
-            });
+            var map = L.map('map').setView([-18.918999, -48.277950], 7);
 
-            var base = new L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-                attribution: '<a href="https://github.com/KyKirma/" target="_blank">Pedro Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Gustavo Corrêa</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. <a href="https://carto.com/attributions" target="_blank">CARTO</a>',
-                subdomains: 'abcd',
-                maxZoom: 15
+            var base = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
+            var attb = '<a href="https://github.com/KyKirma/" target="_blank">Pedro Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Gustavo Corrêa</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. <a href="https://carto.com/attributions" target="_blank">CARTO</a>';
+            L.tileLayer(base, {attribution: attb}).addTo(map);
+
+            new GeoSearch.GeoSearchControl({
+                provider: new GeoSearch.OpenStreetMapProvider(),
+                searchLabel: 'Pesquise um Município',
+                autoClose: true,
+                showMarker: false,
+                style: 'bar'
             }).addTo(map);
-
-            var controlSearch = new L.Control.Search({
-                position: 'topright',
-            });
-
-            map.setView([-18.918999, -48.277950], 7);
-            map.addControl(controlSearch);
-
+              
             for (i = 0; i < response.qnt; i++) {
-                L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
+                marker = L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
                     color: 'red',
                     radius: 5,
                     stroke: false
-                }).bindPopup('<b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área: ' + response.data['AREA_HA'][i] + "Km").addTo(map);
+                });
+
+                marker.bindPopup('<b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área: ' + response.data['AREA_HA'][i] + "Km").addTo(map);
             }
         })
 
