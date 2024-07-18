a0a35e456424fcdfc0184363d73ac268365a28ec
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-07-18 16:55:40 -0300

    Update Search

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -15,9 +15,22 @@ $(document).ready(function () {
 
         .then(function (response) {
             console.log(response);
-
+            
             var map = L.map('map').setView([-18.918999, -48.277950], 7);
+            var attb = '&copy; <a target="_blank" href="https://www.maptiler.com/copyright/">MapTiler</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://github.com/KyKirma/" target="_blank">Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Corrêa</a>';
 
+            const key = 'jlq6npehL8CYWBPs1v4S';
+            
+            L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
+                tileSize: 512,
+                zoomOffset: -1,
+                attribution: attb,
+                crossOrigin: true
+            }).addTo(map);
+
+            L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
+
+            /*
             var base = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
             var attb = '<a href="https://github.com/KyKirma/" target="_blank">Pedro Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Gustavo Corrêa</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. <a href="https://carto.com/attributions" target="_blank">CARTO</a>';
             L.tileLayer(base, {attribution: attb}).addTo(map);
@@ -29,16 +42,18 @@ $(document).ready(function () {
                 showMarker: false,
                 style: 'bar'
             }).addTo(map);
-              
+            */
+
             for (i = 0; i < response.qnt; i++) {
                 marker = L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
-                    color: 'red',
+                    color: 'green',
                     radius: 5,
                     stroke: false
                 });
 
                 marker.bindPopup('<b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área: ' + response.data['AREA_HA'][i] + "Km").addTo(map);
             }
+            
         })
 
         .catch(function (error) {
