ddc76ee7d8d3728efc186c05af8ac32f20c4684e
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-07-18 17:17:07 -0300

    Update Map

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -22,36 +22,23 @@ $(document).ready(function () {
             const key = 'jlq6npehL8CYWBPs1v4S';
             
             L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
+                attribution: attb,
                 tileSize: 512,
                 zoomOffset: -1,
-                attribution: attb,
                 crossOrigin: true
             }).addTo(map);
 
             L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
 
-            /*
-            var base = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
-            var attb = '<a href="https://github.com/KyKirma/" target="_blank">Pedro Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Gustavo Corrêa</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a target="_blank" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>. <a href="https://carto.com/attributions" target="_blank">CARTO</a>';
-            L.tileLayer(base, {attribution: attb}).addTo(map);
-
-            new GeoSearch.GeoSearchControl({
-                provider: new GeoSearch.OpenStreetMapProvider(),
-                searchLabel: 'Pesquise um Município',
-                autoClose: true,
-                showMarker: false,
-                style: 'bar'
-            }).addTo(map);
-            */
-
             for (i = 0; i < response.qnt; i++) {
                 marker = L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
                     color: 'green',
                     radius: 5,
-                    stroke: false
                 });
 
-                marker.bindPopup('<b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área: ' + response.data['AREA_HA'][i] + "Km").addTo(map);
+                marker.bindPopup(
+                    '<b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área: ' + response.data['AREA_HA'][i] + "Km"
+                ).addTo(map);
             }
             
         })
