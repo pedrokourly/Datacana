8a9db4c4592eb94e1cb20db6ee5c5368d6facec7
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-07-19 16:39:15 -0300

    "Refactored map.js and updated dependencies in platform.html"

--- static/scripts/map.js
+++ static/scripts/map.js
@@ -12,14 +12,11 @@ async function getDados() {
 $(document).ready(function () {
     getDados()
         .then(function (response) {
-            console.log(response);
-
             var map = L.map('map', {
                 fullscreenControl: true,
-                fullscreenControlOptions: {
-                    position: 'topleft'
-                }
+                fullscreenControlOptions: { position: 'topleft' }
             }).setView([-18.918999, -48.277950], 7);
+
             var attb = '&copy; <a target="_blank" href="https://www.maptiler.com/copyright/">MapTiler</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://github.com/KyKirma/" target="_blank">Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Corrêa</a>';
 
             const key = 'jlq6npehL8CYWBPs1v4S';
@@ -34,26 +31,25 @@ $(document).ready(function () {
             L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
 
             function getRadius(areaHa) {
-                // Define a escala de raio (ex: 1 hectare = 2 pixels de raio)
+                // Define a escala de raio
                 const scale = 0.15;
 
                 // Calcula o raio com base na área em hectares
                 const radius = (Math.sqrt(areaHa) * scale) / 2;
 
-                // Retorna o raio com um valor mínimo de 2 pixels (opcional)
+                // Retorna o raio com um valor mínimo de 2 pixels
                 return Math.max(radius, 2);
             }
 
-            let marker;
             for (let i = 0; i < response.qnt; i++) {
                 const areaHa = response.data['AREA_HA'][i];
                 const radius = getRadius(areaHa);
 
-                marker = L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
+                let marker = L.circleMarker([response.data['LAT'][i], response.data['LONG'][i]], {
                     color: 'darkgreen',
                     radius: radius,
                     stroke: true,
-                    weight: 0.5,
+                    weight: 0.8,
                     opacity: 1
                 });
 
