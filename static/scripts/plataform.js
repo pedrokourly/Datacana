d6b2f152a9a103e7d2a6c050968df9fee32d00c4
Gustavo Corrêa <ecfgustavo@gmail.com>
2024-07-24 16:35:03 -0300

    "Updated datacana.py to run app on host 0.0.0.0, removed map.js, updated style.css, and refactored platform.html and home.html templates."

--- static/scripts/plataform.js
+++ static/scripts/plataform.js
@@ -12,15 +12,22 @@ async function getDados() {
 $(document).ready(function () {
     getDados()
         .then(function (response) {
+            // Mapa
             var map = L.map('map', {
                 fullscreenControl: true,
                 fullscreenControlOptions: { position: 'topleft' }
-            }).setView([-18.918999, -48.277950], 7);
+            });
 
+            // Visualização
+            map.setView([-18.918999, -48.277950], 7);
+
+            // Atribuição
             var attb = '&copy; <a target="_blank" href="https://www.maptiler.com/copyright/">MapTiler</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a>, <a href="https://github.com/KyKirma/" target="_blank">Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Corrêa</a>';
 
+            // Chave API MapTiler
             const key = 'jlq6npehL8CYWBPs1v4S';
             
+            // Camada Base
             L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
                 attribution: attb,
                 tileSize: 512,
@@ -28,24 +35,30 @@ $(document).ready(function () {
                 crossOrigin: true
             }).addTo(map);
 
-            // Search Map
-            L.control.maptilerGeocoding({ apiKey: key }).addTo(map);
+            // Barra de Pesquisa
+            L.control.maptilerGeocoding({ 
+                apiKey: key 
+            }).addTo(map);
             
+            // Resetar Visualização
             L.control.resetView({
                 position: "topleft",
                 title: "Reset view",
                 latlng: L.latLng([-18.918999, -48.277950]),
-                zoom: 7,
+                zoom: 7
             }).addTo(map);
 
-            var info = L.control({position: 'bottomleft'});
+            // Quadro de Informações 
+            var info = L.control({ 
+                position: 'bottomleft' 
+            });
+
             info.onAdd = function (map) {
                 this._div = L.DomUtil.create('div', 'info');
                 this.update();
                 return this._div;
             };
 
-            // Área Total de Cana
             let totalAreaHa = 0;
             for (let i = 0; i < response.qnt; i++) {
                 totalAreaHa += response.data['AREA_HA'][i];
@@ -66,7 +79,6 @@ $(document).ready(function () {
             function highlightFeature(e) {
                 info.update(e.sourceTarget.options);
             }
-            
             function resetHighlight(e) {
                 info.update();
             }
@@ -74,16 +86,12 @@ $(document).ready(function () {
             info.addTo(map);
 
             function getRadius(areaHa) {
-                // Define a escala de raio
                 const scale = 0.15;
-
-                // Calcula o raio com base na área em hectares
                 const radius = (Math.sqrt(areaHa) * scale) / 2;
-
-                // Retorna o raio com um valor mínimo de 2 pixels
                 return Math.max(radius, 2);
             }
 
+            // Pop-up's Municípios
             for (let i = 0; i < response.qnt; i++) {
                 const municipio = response.data['MUNICIPIO'][i];
                 const areaHa = response.data['AREA_HA'][i];
@@ -96,14 +104,14 @@ $(document).ready(function () {
                     weight: 0.8,
                     opacity: 1,
                     municipio: municipio,
-                    areaHa: areaHa,
+                    areaHa: areaHa
                 });
 
                 var popup = L.responsivePopup().setContent(
                     '<div style="text-align: center;"> <b>' + response.data['MUNICIPIO'][i] + '</b><br>' + 'Área de Cana: ' + response.data['AREA_HA'][i] + " Km² </div>"
                 );
 
-                marker.on({mouseover: highlightFeature, mouseout: resetHighlight});
+                marker.on({ mouseover: highlightFeature, mouseout: resetHighlight});
                 marker.addTo(map).bindPopup(popup);
             }
         })
