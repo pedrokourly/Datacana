fd9915c4f31c20100aea3c942600bc66ce661bf8
Gustavo Corrêa <ecfgustavo@gmail.com>
2025-04-10 22:54:49 -0300

    1st Step of Restructuring
    - General Styles.
    - Component Navbar.
    - Component NavHeader.
    - Component NavCanvas.

--- config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-SECRET_KEY = 'segredo'
-DATAFILES = os.path.dirname(os.path.abspath(__file__)) + '/cache'
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+// Import Style Page Module
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className="Home">
+
+        </div>
+    );
+};
+
+export default Home;
