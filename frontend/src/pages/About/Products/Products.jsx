3292bfb3c3b4d8a949304def26aff7cf87d712fb
Gustavo Corrêa <ecfgustavo@gmail.com>
2025-05-15 00:42:43 -0300

    3rd Step of Restructuring - Scrollbar Custom. - Page Project. - Page Products (Structure). - Page Financing. - Page Team. - Page Terms. - Function to Close OffCanvas.

--- frontend/src/pages/About/Products/Products.jsx
+++ frontend/src/pages/About/Products/Products.jsx
@@ -8,6 +8,20 @@ const Products = () => {
     return (
         <div className="Products">
             <Cover pageName="Produtos"/>
+
+            <div className="content">
+                <div className="container">
+                    <article>
+                        <h2>Sobre os Produtos DataCana</h2>
+
+                        <p>São vários os produtos que os usuários da plataforma podem consultar e realizar o <i>download</i> para os municípios, regiões imediatas e intermediárias de Minas Gerais, acerca da produção canavieira:</p>
+
+                        <h3>Mapas Temáticos</h3>
+                        <h3>Gráficos</h3>
+                        <h3>Tabelas</h3>
+                    </article>
+                </div>
+            </div>
         </div>
     );
 };
