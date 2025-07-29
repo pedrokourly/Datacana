7c8572583f67d78c2fee23e6850feedbb27b61a9
Gustavo Corrêa <ecfgustavo@gmail.com>
2025-07-29 02:15:34 -0300

    6th Step of Restructuring
    - Page Platform (Structure).

--- frontend/src/pages/MapsData/Platform/Platform.jsx
+++ frontend/src/pages/MapsData/Platform/Platform.jsx
@@ -4,10 +4,75 @@ import './Platform.css';
 // Import Images
 import Logo from '/assets/images/DataCana.svg';
 
+// Import React Libs
+import { useEffect, useRef } from 'react';
+
 // Import React Router DOM
 import { NavLink } from 'react-router-dom';
 
 const Platform = () => {
+    // Handling the Leaflet Map
+    useEffect(() => {
+        let map;
+
+        // Check if Map has Already been Initialized
+        if (L.DomUtil.get("map") !== null) {
+            L.DomUtil.get("map")._leaflet_id = null;
+        }
+
+        // Map Variable
+        map = L.map("map");
+        map.setView([-19, -46], 7);
+
+        // Map Attribution
+        const attb = '&copy; <a target="_blank" href="https://www.maptiler.com/copyright/">MapTiler</a>, &copy; <a target = "_blank" href="https://www.openstreetmap.org/">OpenStreetMap</a> | <a href="https://github.com/KyKirma/" target="_blank">Kourly</a>, <a href="https://github.com/gustavomcss" target="_blank">Corrêa</a>'
+
+        // API Key
+        const APIKey = import.meta.env.VITE_MAPTILER_API_KEY;
+
+        // Tile Layers
+        const tileBase = L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${APIKey}`, {
+            attribution: attb,
+            tileSize: 512,
+            zoomOffset: -1,
+            crossOrigin: true
+        }).addTo(map);
+
+        const tileOffColor = L.tileLayer(`https://api.maptiler.com/maps/streets-v2-light/{z}/{x}/{y}.png?key=${APIKey}`, {
+            attribution: attb,
+            tileSize: 512,
+            zoomOffset: -1,
+            crossOrigin: true
+        });
+
+        // Map Control (FullScreen)
+        L.control.fullscreen({
+            position: 'topleft',
+            title: 'Full Screen',
+            titleCancel: 'Exit Full Screen',
+            forceSeparateButton: true
+        }).addTo(map);
+
+        // Map Control (ResetView)
+        L.control.resetView({
+            position: 'topleft',
+            title: 'Reset View',
+            latlng: L.latLng([-19, -46]),
+            zoom: 7
+        }).addTo(map);
+
+        // Map Control (SearchBar)
+        L.control.maptilerGeocoding({
+            apiKey: APIKey
+        }).addTo(map);
+
+        // TODO: Data Processing
+        
+
+        // Cleanup Function
+        return () => { if (map) { map.remove(); } };
+    }, []);
+
     return (
         <div className="Platform">
             <div className="navBar">
@@ -16,7 +81,7 @@ const Platform = () => {
                 </NavLink>
             </div>
 
-            {/* Leaflet Map */}
+            <div id="map"></div>
         </div>
     );
 };
